f75f195313716e094421583187ba84184eec80d8
JoseSalasCalderon <joseant2302@gmail.com>
2025-07-25 21:53:05 -0600

    Arreglo de imágenes y juegos en general
    Se realizó una mejora en la estética de los juegos y de las secciones respectivas, además de que se agregaron las imágenes para los juegos de memoria y rompecabezas respectivamente.

--- src/pages/juegos/rompecabezas.tsx
+++ src/pages/juegos/rompecabezas.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import items from "./datos-juegos/datos-juego-rompecabezas.json";
+import items from "./datos-juegos/datos-juego-memoria-rompecabezas.json";
 
 type PuzzlePiece = {
   id: number;
