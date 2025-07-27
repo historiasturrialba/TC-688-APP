ba42f3c8e812ce47f7e8ac4758601321acd40eaf
Josue19-08 <josuemarin2009@hotmail.com>
2025-07-27 14:36:45 -0600

    Agregar datos reales de personajes importantes

---
+++ src/data/personajes-data.ts
@@ -0,0 +1,49 @@
+const personajesData = [
+  {
+    id: 1,
+    nombre: "Don Benjamin Calvo",
+    categoria: "Historia Local",
+    fotoUrl: "/historia/personajes-importantes/benjamin-calvo.webp",
+    descripcion: `Él vive en el Repasto calle 1 pero nació y vivió parte de su infancia en La Isabel. La foto fue tomada aproximadamente en 1970, en el puente de La Isabel. Se puede ver el río y el beneficio de café, además de algunas casas que ya no existen.\n
+    Hijo de Jerónimo Calvo, quien fue el primero en poner una pulpería en la comunidad.`,
+    labor: "",
+    año: "1970",
+  },
+  {
+    id: 2,
+    nombre: "Antonia Sánchez Molina",
+    categoria: "Labor Comunitaria",
+    fotoUrl: "/historia/personajes-importantes/antonia-sanchez.webp",
+    descripcion:
+      "Partera y sobadora de la comunidad. Nacida el 01/06/1927. Vivió 86 años.",
+    labor: "Partera y sobadora",
+    año: "1927 - 2013",
+  },
+  {
+    id: 3,
+    nombre: 'Integrantes del grupo musical "Los Miller"',
+    categoria: "Música",
+    fotoUrl: "/historia/personajes-importantes/los-miller.webp",
+    descripcion: `Foto tomada frente a la casa de los doctores por Margoth, mientras posaban con el carro que los transportaba a eventos. Walter, cantante, es el único vivo.\n
+    Gilbert (cantante y timbales), otros integrantes: batería, requinto, organeta y bajista. Algunos son familiares.`,
+    labor: "Músicos",
+    año: "Aprox. 30 años atrás",
+  },
+  {
+    id: 4,
+    nombre: "Mario Picado",
+    categoria: "Educación y Literatura",
+    fotoUrl: [
+      "/historia/personajes-importantes/mario-picado1.webp",
+      "/historia/personajes-importantes/mario-picado2.webp",
+    ],
+    descripcion: `Reconocido educador y poeta costarricense que vivió unos cinco años en la comunidad. Destacado por su amor a la literatura y su poesía.\n
+    Fue galardonado con premios nacionales Aquilejo y Cherré (3 veces).\n
+    Murió joven a los 59 años, dejando un legado poético que aún se recuerda.\n
+    Información basada en entrevista y documentos realizados por Heilin Córdoba y Rolando Picado González.`,
+    labor: "Educador y poeta",
+    año: "Fallecido a los 59 años",
+  }
+];
+
+export default personajesData;
