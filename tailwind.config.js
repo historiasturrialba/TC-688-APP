064e89c6aed40a8b106ebd8fadf316ac9f347c87
Josue19-08 <josuemarin2009@hotmail.com>
2025-07-27 12:26:27 -0600

    Modifica tailwind.config.js tipado para configuración de estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,29 @@ module.exports = {
         border: "hsl(var(--border))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        'midnight': '#0C2340',
+        'coconut-cream': '#FDF8E8',
+        'shadow': '#866556',
+        'hemlock': '#565E42',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0px) rotate(-5deg)' },
+          '50%': { transform: 'translateY(-20px) rotate(5deg)' },
+        },
+        'float-reverse': {
+          '0%, 100%': { transform: 'translateY(0px) rotate(5deg)' },
+          '50%': { transform: 'translateY(-20px) rotate(-5deg)' },
+        },
+        peek: {
+          '0%, 100%': { transform: 'translateY(20px)' },
+          '50%': { transform: 'translateY(0px)' },
+        },
+      },
+      animation: {
+        'float': 'float 4s ease-in-out infinite',
+        'float-reverse': 'float-reverse 4s ease-in-out infinite',
+        'peek': 'peek 3s ease-in-out infinite',
       },
     },
   },
