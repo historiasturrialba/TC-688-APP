7369bdcb65a041bc8d0da5e643467598b19433f3
sanchez1999 <sanchezalfredo590@gmail.com>
2025-07-27 18:47:20 -0600

    Update back-button-game.tsx

--- src/components/origen-de/back-button-game.tsx
+++ src/components/origen-de/back-button-game.tsx
@@ -4,7 +4,7 @@ import { ArrowLeft } from "lucide-react"
 export default function BackButtonGame() {
   return (
     <Link
-      href="/historia"
+      href="/juegos"
       className="inline-flex items-center text-hemlock hover:text-midnight mb-6 transition-all duration-300 group"
     >
       <ArrowLeft className="h-4 w-4 mr-2 transition-transform group-hover:-translate-x-1" />
